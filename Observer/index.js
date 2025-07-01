83c60496803753a422bf40e40734eab7e83c146e
training-dev <contact@training-dev.fr>
2025-07-01 16:17:35 +0200

    fix button event

--- Observer/index.js
+++ Observer/index.js
@@ -3,9 +3,9 @@ import Banque from "./Banque.js";
 let banque = new Banque();
 
 
-document.querySelector('#withdraw').addEventListener('click', (e) => {
+document.querySelectorAll('.withdraw').forEach(btn => btn.addEventListener('click', (e) => {
     banque.DAB.find(dab => dab.name === e.target.dataset.name).withdraw(250)
-});
-document.querySelector('#deposite').addEventListener('click', (e) => {
+}));
+document.querySelectorAll('.deposite').forEach(btn => btn.addEventListener('click', (e) => {
     banque.DAB.find(dab => dab.name === e.target.dataset.name).deposite(100)
-});
+}));
